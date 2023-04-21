017526ffb5aea8cf58b69c189634280b9ea48186
Gieo Coronado <coronadogieo@gmail.com>
2023-04-21 16:32:49 +0800

    updated link to repo

--- src/components/Infopopup.js
+++ src/components/Infopopup.js
@@ -19,7 +19,7 @@ function Infopopup(){
                         <p className="title-info">your (un)helpful filo motivator</p>
                         <img className="idpic" src={myPhoto} alt="Gieo Coronado"></img>
                         <p className="">This web-application is created using ReactJS and Flask by Gieo Coronado</p>
-                        <a style={{textDecoration: 'underline'}} onClick={()=>{alert("To be updated soon...")}}>view github repository</a>
+                        <a href='https://github.com/coronadogieo/your-filo-motivator' target="_blank">View github repository</a>
                     </div>
                     <div className="btn-close-info-div">
                         <button className="btn-close-info" onClick={togglePopup}>close</button>
